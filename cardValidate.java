1465a36f309f34db5411e9b4edf0ccb31025dac6
wow <404346161@mail.fju.edu.tw>
2015-12-06 14:13:00 +0800

    增加註解

--- cardValidate.java
+++ cardValidate.java
@@ -10,7 +10,7 @@ if(isValid(input.nextLong())){
 }
 
 
-
+//驗證number是否Valid，如果是valid回傳true 否則回傳false
 private static boolean isValid(long number){
 	if(getPrefixMatched(getPrefix(number)) != true){
 		System.out.println("WRONG PREFIX NUMBER");
@@ -25,13 +25,15 @@ private static boolean isValid(long number){
 
 }
 
+
+//獲得奇數位的總和
 private static long getOddNumber(Long number){
 	long calcuNum=0,result = 0; 
 	for(int i = 0 ; i <= getSize(number)-1 ; i = i + 2)
 	{	calcuNum = 0;
 		calcuNum = number / (long)Math.pow(10,i);
 		calcuNum = (calcuNum % 10);
-		if(calcuNum>=5)
+		if(calcuNum>=5)  //因為大於5 * 2 就會超過兩位數，所以直接用switch 來 mapping 所有兩位數的最後結果
 			switch ((int)calcuNum){
 				case 5:
 					calcuNum =1L;break;
@@ -52,7 +54,7 @@ private static long getOddNumber(Long number){
 }
 
 
-
+//偶數位的數字總和，結果會以int的方式回傳
 private static long getEvenNumber(Long number){
 	long calcuNum=0,result = 0;
 	for(int i = 1 ; i <= getSize(number)-1 ; i = i + 2)
@@ -79,6 +81,8 @@ private static long getEvenNumber(Long number){
 	return result;
 
 }
+
+//獲得number的長度，可以用這個方法來推論他是幾位數的number
 private static int getSize(long number){
 	int count = 0;
 	while(number > 0){
@@ -87,6 +91,8 @@ private static int getSize(long number){
 	}
 	return count;
 }
+
+//驗證信用卡的prefix number是否為 37/4/5/6這幾種....
 private static boolean getPrefixMatched(int number){
 	if(number == 37)
 		return true;
@@ -104,7 +110,7 @@ private static boolean getPrefixMatched(int number){
 		}
 }
 
-
+//取得信用卡的prefix number ，此處取最開頭的兩個數字
 private static int getPrefix(long number){
 	int thisSize = getSize(number); //檢查數字長度
 	number = number / (long)Math.pow(10,thisSize-2);
